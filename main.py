581430bf67e21447ddd1bdb7d7f9369a79dd605d
noorie.song <noorie.song@cdnetworks.com>
2019-11-20 17:45:04 +0900

    second commit

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ bldg_code = 8458
 param = {
   'hscpNo' : bldg_code,
   'tradTpCd' : 'A1:B1:B2:B3', #A1 매매, B1 전세, B2 월세, B3 단기임대
-  'sortCd' : 'RANK', #RANK, RECNT
-  'ptpNo': 1,
-  'order' : 'point_' #point 랭킹순, prc 가격순, 최신순 date, 면적순 spc
+   'showR0': 'N',
+  'articleListYN' : 'Y',
+  'order' : 'date_' #point 랭킹순, prc 가격순, 최신순 date, 면적순 spc
 }
 
 header = {
@@ -30,6 +30,8 @@ header = {
 logging.basicConfig(level= logging.INFO)
 page = 0
 
+excel_header = ['type', 'building', '호수', '면적', 'price', 'min_price', 'max_price', 'date', 'realtor', 'cnt_realtors']
+excel_result = []
 
 while True :
   page += 1
@@ -47,43 +49,40 @@ while True :
     logging.error ('No Data')
     break
 
-  excel_header = ['type', 'building', '호수', 'price', 'min_price', 'max_price', 'realtor', 'cnt_realtor']
-  excel_result = []
-
   for item in result['list'] :
     # if float(item['spc2']) < 80 or float(item['spc2'] > 85) :
     #   continue
     append_data = ( item['tradTpNm'].encode('utf-8'),
                                    item['bildNm'].encode('utf-8'),
                                    item['flrInfo'].encode('utf-8'),
+                                   item['spc2'],
                                    item['prcInfo'].encode('utf-8'),
                                    item['sameAddrMinPrc'].encode('utf-8'),
                                    item['sameAddrMaxPrc'].encode('utf-8'),
+                                   item['cfmYmd'].encode('utf-8'),
                                    item['rltrNm'].encode('utf-8'),
                                    item['sameAddrCnt'] )
     excel_result.append(append_data)
 
-
-  import csv
-  import codecs
-  from datetime import datetime
-
-  time = datetime.now()
-  time = time.strftime('%Y-%m-%d')
-  print time
-
-  csvfile = open('./%s_bldg_code.csv' %time, 'w')
-  csvfile.write(codecs.BOM_UTF8)
-  writer = csv.writer(csvfile, delimiter=',')
-  writer.writerow(excel_header)
-
-  for excel in excel_result :
-    writer.writerow(excel)
-
-  csvfile.close()
-
-  print("Success")
-
   if result['moreDataYn'] == 'N' :
     break
+
+import csv
+import codecs
+from datetime import datetime
+
+time = datetime.now()
+time = time.strftime('%Y-%m-%d')
+print (time)
+
+csvfile = open('./%s_%s.csv' % (time, bldg_code), 'w')
+csvfile.write(codecs.BOM_UTF8)
+writer = csv.writer(csvfile, delimiter=',')
+writer.writerow(excel_header)
+
+for excel in excel_result:
+  writer.writerow(excel)
+
+csvfile.close()
+print("Success")
 
